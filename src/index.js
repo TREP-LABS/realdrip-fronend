205e85af910a6c3b0b8fc1947abd046154e7a3c9
Tunmise Ogunniyi <ogunniyitunmise@gamil.com>
2019-09-22 20:28:59 +0100

    Implement signup components, actions, and reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import Routes from './Routes';
-import store from './store';
+import Routes from './components/routes';
+import configureStore from './configureStore';
+import './styles.css';
+
+const store = configureStore();
 
 render(
     <Provider store={store}>
