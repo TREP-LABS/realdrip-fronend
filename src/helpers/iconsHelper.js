cb9df90520c2798d9472a15a137b0e8468fe5f54
gbengaPS <ioyetade@gmail.com>
2019-10-28 10:33:13 +0100

    add import helper for icons

---
+++ src/helpers/iconsHelper.js
@@ -0,0 +1,10 @@
+import { ReactComponent as dashboard} from '../icons/dashboard.svg';
+import { ReactComponent as settings} from '../icons/settings.svg';
+import { ReactComponent as operations} from '../icons/operations.svg';
+import { ReactComponent as devices} from '../icons/devices.svg';
+import { ReactComponent as nurses} from '../icons/nurse.svg';
+
+
+const icons = { dashboard, settings, operations, devices, nurses };
+
+export default icons;
