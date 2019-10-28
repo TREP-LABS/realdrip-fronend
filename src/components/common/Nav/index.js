75fe2077b9dc0d5b887abf8733fc64e6ff516794
gbengaPS <ioyetade@gmail.com>
2019-10-28 10:30:36 +0100

    create Nav component

---
+++ src/components/common/Nav/index.js
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import Label from '../Label';
+import './nav.scss';
+
+class Nav extends PureComponent {
+  render(){
+    return (
+        <nav className="top-nav">
+        <div className="app-container">
+          <div className="nav-items">
+            <div><Label value="Ward"/></div>
+            <div><span>How it works</span></div>
+          </div>
+        </div>
+        </nav>
+     )
+  }
+}
+
+export default Nav;
